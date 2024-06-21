e27aaf1b0322a774cdfa538e09cc3e706d10eec9
mrsyafapri <rizkisyafapri@gmail.com>
2024-06-21 11:56:01 +0700

    feat: hash password before saving

--- models/employee.js
+++ models/employee.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const employeeSchema = new mongoose.Schema({
     name: {
@@ -39,5 +40,14 @@ const employeeSchema = new mongoose.Schema({
 
 // Indexing the email field for faster query performance
 employeeSchema.index({ email: 1 });
+
+// Pre-save middleware to hash password before saving
+employeeSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
 
 module.exports = mongoose.model('Employee', employeeSchema);
